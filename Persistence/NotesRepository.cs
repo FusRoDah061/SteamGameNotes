66075427ebb6a0209864370fd943db225fe0f5ab
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-09-28 19:22:08 -0300

    Fix error removing game with no notes

--- Persistence/NotesRepository.cs
+++ Persistence/NotesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -6,8 +7,15 @@ namespace SteamGameNotes.Persistence
 {
     public class NotesRepository
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(NotesRepository));
+
         private const string NOTE_FILENAME = "content.txt";
         private string BASE_DIRECTORY = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\SteamGameNotes\\notes";
+
+        public NotesRepository()
+        {
+            log.Info("Notes will be saved to " + Path.Combine(BASE_DIRECTORY, "<appid>", NOTE_FILENAME));
+        }
 
         public async Task SaveNotes(long appId, string note)
         {
@@ -23,7 +31,14 @@ namespace SteamGameNotes.Persistence
 
         public void Delete(long appId)
         {
-            File.Delete(Path.Combine(BASE_DIRECTORY, appId.ToString(), NOTE_FILENAME));
+            try
+            {
+                File.Delete(Path.Combine(BASE_DIRECTORY, appId.ToString(), NOTE_FILENAME));
+            } 
+            catch(Exception ex)
+            {
+                log.Warn($"Error deleting file {Path.Combine(BASE_DIRECTORY, appId.ToString(), NOTE_FILENAME)}: ", ex);
+            }
         }
     }
 }
